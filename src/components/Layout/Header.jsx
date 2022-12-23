626841682aa74f309792228b8e256e1a8d70410c
Jacel <jacelcamargo@gmail.com>
2022-12-23 13:33:45 -0300

    first commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>CamargoReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of a delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
